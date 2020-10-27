31429b284a02ac41279a65e1b4b77432ab201616
divshah <divshah@paypal.com>
2020-10-26 22:21:52 -0700

    added article example

---
+++ src/pages/articles/example.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const NotFoundPage = () => (
+  <Layout>
+    <h1>Article example</h1>
+  </Layout>
+)
+
+export default NotFoundPage
